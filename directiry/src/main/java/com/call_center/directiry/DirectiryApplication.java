14def4fd00ec7e48030d6ce2713585fdb3f15f92
Timur <i_laked@bk.ru>
2024-11-17 16:12:07 +0300

    test

---
+++ directiry/src/main/java/com/call_center/directiry/DirectiryApplication.java
@@ -0,0 +1,13 @@
+package com.call_center.directiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectiryApplication.class, args);
+	}
+
+}
---
+++ directiry/src/test/java/com/call_center/directiry/DirectiryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.call_center.directiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
